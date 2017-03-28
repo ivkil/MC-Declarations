ea48ba3f0e017e2bf834d40e0d35dd0c6f3f6015
Ivan Kiliian <ivan@kiliian.com>
2017-03-28 22:59:08 +0300

    get mc from csv: update if exists

--- councils_members/management/commands/get_mc.py
+++ councils_members/management/commands/get_mc.py
@@ -13,7 +13,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open(options['path'][0], encoding='UTF-8') as f:
             mc_reader = csv.DictReader(f)
+            index = 0
             for row in mc_reader:
+                index += 1
+                self.stdout.write("Working on row %s" % index)
                 region = Region.objects.get_or_create(title=row['region'])[0]
                 council_type = CouncilType.objects.get_or_create(title=row['council_type'])[0]
                 councils = Council.objects.filter(region__id=region.id, type__id=council_type.id,
@@ -23,9 +26,9 @@ class Command(BaseCommand):
                 else:
                     council = Council.objects.create(region=region, type=council_type, title=row['council'])
 
-                mc = MemberCouncil.objects.create(
+                q = MemberCouncil.objects.filter(
                     name=row['cm_name'],
-                    council=council,
+                    council__id=council.id,
                     citizenship=row['cm_citizenship'],
                     date_of_birth=datetime.datetime.strptime(row['cm_date_of_birth'], '%Y-%m-%d'),
                     education=row['cm_education'],
@@ -33,4 +36,16 @@ class Command(BaseCommand):
                     workplace=row['cm_workplace'],
                     residence=row['cm_residence']
                 )
-                self.stdout.write(str(mc))
+                if not q.exists():
+                    mc = MemberCouncil.objects.create(
+                        name=row['cm_name'],
+                        council=council,
+                        citizenship=row['cm_citizenship'],
+                        date_of_birth=datetime.datetime.strptime(row['cm_date_of_birth'], '%Y-%m-%d'),
+                        education=row['cm_education'],
+                        party=row['cm_party'],
+                        workplace=row['cm_workplace'],
+                        residence=row['cm_residence']
+                    )
+                    self.stdout.write("Create: " + str(mc))
+            self.stdout.write("The end")
