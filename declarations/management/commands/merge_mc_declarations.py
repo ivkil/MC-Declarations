3c7249a3ffc47e839400be986ed74366247aa43c
Ivan Kiliian <ivan@kiliian.com>
2017-03-28 18:11:55 +0300

    update merge mc with declarations

--- declarations/management/commands/merge_mc_declarations.py
+++ declarations/management/commands/merge_mc_declarations.py
@@ -1,5 +1,7 @@
+import datetime
 import re
-import requests
+
+import grequests
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
 
@@ -30,20 +32,31 @@ class Command(BaseCommand):
         index = 0
         for declaration in declarations:
             index += 1
-            self.stdout.write("Working with declaration %s of %s total" % (index, total))
+            self.stdout.write("Working on declaration %s of %s total" % (index, total))
             name = declaration.last_name + " " + declaration.first_name
             q = MemberCouncil.objects.filter(name__icontains=name)
             if q.exists():
-                r = requests.get("https://declarations.com.ua/declaration/nacp_%s" % declaration.id).text
-                declaration.residence = residence(r)
-                for mc in q:
-                    declaration.residence = residence(r)
-                    if same_residence(mc.residence, declaration.residence):
-                        mc.declaration = declaration
-                        mc.save()
-                        count += 1
-                        self.stdout.write("Merged: " + declaration.residence + " - " + mc.residence)
-                        break
-            declaration.checked = True
-            declaration.save()
+                rs = (grequests.get(u) for u in get_urls(declaration.id))
+                resps = grequests.map(rs)
+                if resps[0].status_code == 200 & resps[1].status_code == 200:
+                    txt = resps[0].text
+                    declaration.residence = residence(txt)
+                    declaration.decl_json = resps[1].json()
+                    d = datetime.datetime.strptime(declaration.decl_json["declaration"]["intro"]["date"],
+                                                   "%Y-%m-%dT%H:%M:%S")
+                    if d >= datetime.datetime(2017, 1, 1):
+                        for mc in q:
+                            if same_residence(mc.residence, declaration.residence):
+                                mc.declaration = declaration
+                                mc.save()
+                                count += 1
+                                self.stdout.write("Merged: " + declaration.residence + " - " + mc.residence)
+                                break
+                    declaration.checked = True
+                    declaration.save()
         self.stdout.write("Finished. %s declarations have been merged" % count)
+
+
+def get_urls(declaration_id):
+    return ["https://declarations.com.ua/declaration/nacp_%s" % declaration_id,
+            "https://declarations.com.ua/declaration/nacp_%s?format=json" % declaration_id]
