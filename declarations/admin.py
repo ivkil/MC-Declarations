8b8919a7ae4b0bfb0b5f0bed4489b166be5fcac6
Ivan Kiliian <ivan@kiliian.com>
2017-04-01 14:26:13 +0300

    include declaration json to admin page

--- declarations/admin.py
+++ declarations/admin.py
@@ -6,7 +6,6 @@ from declarations.models import Declaration
 class DeclarationAdmin(admin.ModelAdmin):
     search_fields = ('id', 'last_name', 'first_name', 'workplace', 'position', 'residence')
     readonly_fields = ('checked',)
-    exclude = ('decl_json',)
 
 
 admin.site.register(Declaration, DeclarationAdmin)
