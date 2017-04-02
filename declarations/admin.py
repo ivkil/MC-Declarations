0b3a6911a31955e8b1a31ff0ee78c8fd18179b14
Ivan Kiliian <ivan@kiliian.com>
2017-04-02 19:52:16 +0300

    declarations admin filter by checked

--- declarations/admin.py
+++ declarations/admin.py
@@ -5,6 +5,7 @@ from declarations.models import Declaration
 
 class DeclarationAdmin(admin.ModelAdmin):
     search_fields = ('id', 'last_name', 'first_name', 'workplace', 'position', 'residence')
+    list_filter = ('checked',)
     readonly_fields = ('checked',)
 
 
